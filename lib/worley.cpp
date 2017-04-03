5737d4f24259a7c380fb1b24368fb7e94fafb070
Philip Woods <elzairthesorcerer@gmail.com>
2017-04-03 18:44:23 -0400

    Implemented 2D 3D and 4D Worley Noise

--- lib/worley.cpp
+++ lib/worley.cpp
@@ -42,7 +42,7 @@ namespace Noise
 
         for ( int64_t i = -1; i <= 1; i++ )
         {
-            auto xc  = xi + i;
+            auto xc = xi + i;
 
             // Generate reproducible random number
             auto xs0 = this->seed;
@@ -52,7 +52,7 @@ namespace Noise
             auto numfp = this->lookupNumFeaturePoints( Util::RNG( xs0, xs1 ) );
 
             // Distribute feature points randomly around cell.
-            for ( auto l = 0; l < numfp; l++ )
+            for ( auto m = 0; m < numfp; m++ )
             {
                 real_t fp = xf + Util::U64ToNormReal( Util::RNG( xs0, xs1 ) );
                 auto dist = 0.0;
@@ -71,7 +71,9 @@ namespace Noise
                 }
 
                 // Keep track of the N closest feature points to the point x.
-                Util::InsertAndSort( distances.data(), Worley1D::DISTANCES_SIZE, dist );
+                Util::InsertAndSort( distances.data(),
+                                     Worley1D::DISTANCES_SIZE,
+                                     dist );
             }
         }
 
@@ -81,6 +83,328 @@ namespace Noise
     std::size_t Worley1D::lookupNumFeaturePoints( uint64_t num ) const
     {
         for ( auto i = Worley1D::MAX_PER_CELL-1; i > 0; i-- )
+        {
+            if ( num > this->probTable[i] )
+            {
+                return i;
+            }
+        }
+    }
+
+    real_t Worley2D::getValue( real_t x, real_t y ) const
+    {
+        auto distances = this->getValueHelper( x, y );
+        
+        // Return the distance to the closest feature point.
+        return distances[0];
+    }
+
+    real_t Worley2D::getCustomValue(
+        real_t                                                                x,
+        real_t                                                                y,
+        std::function<real_t( std::array<real_t, Worley2D::DISTANCES_SIZE> )> fn
+    )
+    {
+        auto distances = this->getValueHelper( x, y );
+
+        return fn( distances );
+    }
+
+    std::array<real_t, Worley2D::DISTANCES_SIZE>
+    Worley2D::getValueHelper( real_t x, real_t y ) const
+    {
+        std::array<real_t, Worley2D::DISTANCES_SIZE> distances;
+
+        for ( auto& distance : distances )
+        {
+            distance = 1.0e12; // Initialize distances to large value.
+        }
+
+        // Determine the unit location of point.
+        real_t  xf = std::floor( x );
+        int64_t xi = static_cast<int64_t>( xf );
+        real_t  yf = std::floor( y );
+        int64_t yi = static_cast<int64_t>( yf );
+
+        for ( int64_t i = -1; i <= 1; i++ )
+        {
+            for ( int64_t j = -1; j <= 1; j++ )
+            {
+                auto xc = xi + i;
+                auto yc = yi + j;
+
+                // Generate reproducible random number
+                auto xs0 = this->seed;
+                auto xs1 = Util::Hash( Util::I64ToU64( xc ),
+                                       Util::I64ToU64( yc ) );
+
+                // Determine # feature points in this cell.
+                auto numfp = this->lookupNumFeaturePoints( Util::RNG( xs0, xs1 ) );
+
+                // Distribute feature points randomly around cell.
+                for ( auto m = 0; m < numfp; m++ )
+                {
+                    real_t xfp  = xf + Util::U64ToNormReal( Util::RNG( xs0, xs1 ) );
+                    real_t yfp  = yf + Util::U64ToNormReal( Util::RNG( xs0, xs1 ) );
+                    auto   dist = 0.0;
+
+                    if ( this->distCalcMethod == Distance::Euclidian )
+                    {
+                        dist = ( x - xfp ) * ( x - xfp ) +
+                               ( y - yfp ) * ( y - yfp );
+                    }
+                    else if ( this->distCalcMethod == Distance::Manhattan )
+                    {
+                        dist = std::abs( x - xfp ) +
+                               std::abs( y - yfp );
+                    }
+                    else if ( this->distCalcMethod == Distance::Chebyshev )
+                    {
+                        auto xd = std::abs( x - xfp );
+                        auto yd = std::abs( y - yfp );
+                        dist    = std::max( xd, yd );
+                    }
+
+                    // Keep track of the N closest feature points to the point x.
+                    Util::InsertAndSort( distances.data(),
+                                         Worley2D::DISTANCES_SIZE,
+                                         dist );
+                }
+            }
+        }
+
+        return distances;
+    }
+
+    std::size_t Worley2D::lookupNumFeaturePoints( uint64_t num ) const
+    {
+        for ( auto i = Worley2D::MAX_PER_CELL-1; i > 0; i-- )
+        {
+            if ( num > this->probTable[i] )
+            {
+                return i;
+            }
+        }
+    }
+
+    real_t Worley3D::getValue( real_t x, real_t y, real_t z ) const
+    {
+        auto distances = this->getValueHelper( x, y, z );
+        
+        // Return the distance to the closest feature point.
+        return distances[0];
+    }
+
+    real_t Worley3D::getCustomValue(
+        real_t                                                                x,
+        real_t                                                                y,
+        real_t                                                                z,
+        std::function<real_t( std::array<real_t, Worley3D::DISTANCES_SIZE> )> fn
+    )
+    {
+        auto distances = this->getValueHelper( x, y, z );
+
+        return fn( distances );
+    }
+
+    std::array<real_t, Worley3D::DISTANCES_SIZE>
+    Worley3D::getValueHelper( real_t x, real_t y, real_t z ) const
+    {
+        std::array<real_t, Worley3D::DISTANCES_SIZE> distances;
+
+        for ( auto& distance : distances )
+        {
+            distance = 1.0e12; // Initialize distances to large value.
+        }
+
+        // Determine the unit location of point.
+        real_t  xf = std::floor( x );
+        int64_t xi = static_cast<int64_t>( xf );
+        real_t  yf = std::floor( y );
+        int64_t yi = static_cast<int64_t>( yf );
+        real_t  zf = std::floor( z );
+        int64_t zi = static_cast<int64_t>( zf );
+
+        for ( int64_t i = -1; i <= 1; i++ )
+        {
+            for ( int64_t j = -1; j <= 1; j++ )
+            {
+                for ( int64_t k = -1; k <= 1; k++ )
+                {
+                    auto xc = xi + i;
+                    auto yc = yi + j;
+                    auto zc = zi + k;
+
+                    // Generate reproducible random number
+                    auto xs0 = this->seed;
+                    auto xs1 = Util::Hash( Util::I64ToU64( xc ),
+                                           Util::I64ToU64( yc ),
+                                           Util::I64ToU64( zc ) );
+
+                    // Determine # feature points in this cell.
+                    auto numfp = this->lookupNumFeaturePoints( Util::RNG( xs0, xs1 ) );
+
+                    // Distribute feature points randomly around cell.
+                    for ( auto m = 0; m < numfp; m++ )
+                    {
+                        real_t xfp  = xf + Util::U64ToNormReal( Util::RNG( xs0, xs1 ) );
+                        real_t yfp  = yf + Util::U64ToNormReal( Util::RNG( xs0, xs1 ) );
+                        real_t zfp  = zf + Util::U64ToNormReal( Util::RNG( xs0, xs1 ) );
+                        auto   dist = 0.0;
+
+                        if ( this->distCalcMethod == Distance::Euclidian )
+                        {
+                            dist = ( x - xfp ) * ( x - xfp ) +
+                                   ( y - yfp ) * ( y - yfp ) +
+                                   ( z - zfp ) * ( z - zfp );
+                        }
+                        else if ( this->distCalcMethod == Distance::Manhattan )
+                        {
+                            dist = std::abs( x - xfp ) +
+                                   std::abs( y - yfp ) +
+                                   std::abs( z - zfp );
+                        }
+                        else if ( this->distCalcMethod == Distance::Chebyshev )
+                        {
+                            auto xd = std::abs( x - xfp );
+                            auto yd = std::abs( y - yfp );
+                            auto zd = std::abs( z - zfp );
+                            dist    = std::max( xd, std::max( yd, zd ) );
+                        }
+
+                        // Keep track of the N closest feature points to the point x.
+                        Util::InsertAndSort( distances.data(), Worley3D::DISTANCES_SIZE, dist );
+                    }
+                }
+            }
+        }
+
+        return distances;
+    }
+
+    std::size_t Worley3D::lookupNumFeaturePoints( uint64_t num ) const
+    {
+        for ( auto i = Worley3D::MAX_PER_CELL-1; i > 0; i-- )
+        {
+            if ( num > this->probTable[i] )
+            {
+                return i;
+            }
+        }
+    }
+
+    real_t Worley4D::getValue( real_t x, real_t y, real_t z, real_t w ) const
+    {
+        auto distances = this->getValueHelper( x, y, z, w );
+        
+        // Return the distance to the closest feature point.
+        return distances[0];
+    }
+
+    real_t Worley4D::getCustomValue(
+        real_t                                                                x,
+        real_t                                                                y,
+        real_t                                                                z,
+        real_t                                                                w,
+        std::function<real_t( std::array<real_t, Worley4D::DISTANCES_SIZE> )> fn
+    )
+    {
+        auto distances = this->getValueHelper( x, y, z, w );
+
+        return fn( distances );
+    }
+
+    std::array<real_t, Worley4D::DISTANCES_SIZE>
+    Worley4D::getValueHelper( real_t x, real_t y, real_t z, real_t w ) const
+    {
+        std::array<real_t, Worley4D::DISTANCES_SIZE> distances;
+
+        for ( auto& distance : distances )
+        {
+            distance = 1.0e12; // Initialize distances to large value.
+        }
+
+        // Determine the unit location of point.
+        real_t  xf = std::floor( x );
+        int64_t xi = static_cast<int64_t>( xf );
+        real_t  yf = std::floor( y );
+        int64_t yi = static_cast<int64_t>( yf );
+        real_t  zf = std::floor( z );
+        int64_t zi = static_cast<int64_t>( zf );
+        real_t  wf = std::floor( w );
+        int64_t wi = static_cast<int64_t>( wf );
+
+        for ( int64_t i = -1; i <= 1; i++ )
+        {
+            for ( int64_t j = -1; j <= 1; j++ )
+            {
+                for ( int64_t k = -1; k <= 1; k++ )
+                {
+                    for ( int64_t l = -1; l <= 1; l++ )
+                    {
+                        auto xc = xi + i;
+                        auto yc = yi + j;
+                        auto zc = zi + k;
+                        auto wc = wi + l;
+
+                        // Generate reproducible random number
+                        auto xs0 = this->seed;
+                        auto xs1 = Util::Hash( Util::I64ToU64( xc ),
+                                               Util::I64ToU64( yc ),
+                                               Util::I64ToU64( zc ),
+                                               Util::I64ToU64( wc ) );
+
+                        // Determine # feature points in this cell.
+                        auto numfp = this->lookupNumFeaturePoints( Util::RNG( xs0, xs1 ) );
+
+                        // Distribute feature points randomly around cell.
+                        for ( auto m = 0; m < numfp; m++ )
+                        {
+                            real_t xfp  = xf + Util::U64ToNormReal( Util::RNG( xs0, xs1 ) );
+                            real_t yfp  = yf + Util::U64ToNormReal( Util::RNG( xs0, xs1 ) );
+                            real_t zfp  = zf + Util::U64ToNormReal( Util::RNG( xs0, xs1 ) );
+                            real_t wfp  = wf + Util::U64ToNormReal( Util::RNG( xs0, xs1 ) );
+                            auto   dist = 0.0;
+
+                            if ( this->distCalcMethod == Distance::Euclidian )
+                            {
+                                dist = ( x - xfp ) * ( x - xfp ) +
+                                       ( y - yfp ) * ( y - yfp ) +
+                                       ( z - zfp ) * ( z - zfp ) +
+                                       ( w - wfp ) * ( w - wfp );
+                            }
+                            else if ( this->distCalcMethod == Distance::Manhattan )
+                            {
+                                dist = std::abs( x - xfp ) +
+                                       std::abs( y - yfp ) +
+                                       std::abs( z - zfp ) +
+                                       std::abs( w - wfp );
+                            }
+                            else if ( this->distCalcMethod == Distance::Chebyshev )
+                            {
+                                auto xd = std::abs( x - xfp );
+                                auto yd = std::abs( y - yfp );
+                                auto zd = std::abs( z - zfp );
+                                auto wd = std::abs( w - wfp );
+                                dist    = std::max( xd, std::max( yd, std::max( zd, wd ) ) );
+                            }
+
+                            // Keep track of the N closest feature points to the point x.
+                            Util::InsertAndSort( distances.data(),
+                                                 Worley4D::DISTANCES_SIZE,
+                                                 dist );
+                        }
+                    }
+                }
+            }
+        }
+
+        return distances;
+    }
+
+    std::size_t Worley4D::lookupNumFeaturePoints( uint64_t num ) const
+    {
+        for ( auto i = Worley4D::MAX_PER_CELL-1; i > 0; i-- )
         {
             if ( num > this->probTable[i] )
             {
--- lib/worley.h
+++ lib/worley.h
@@ -64,4 +64,166 @@ namespace Noise
 
         std::size_t lookupNumFeaturePoints( uint64_t num ) const;
     };
+    
+    class Worley2D : public Module2D
+    {
+    public:
+
+        static const std::size_t DISTANCES_SIZE = 4;
+
+        Worley2D( uint64_t seed           = 0,
+                  real_t   avgPerCell     = 2.0,
+                  Distance distCalcMethod = Distance::Euclidian ) :
+            seed( seed ),
+            avgPerCell( avgPerCell ),
+            distCalcMethod( distCalcMethod )
+        {
+            // Use the Cumulative Distribution Function of the Poisson Distribution
+            // to make a table on the odds of each cell having exactly n feature points.
+            // https://en.wikipedia.org/wiki/Poisson_distribution
+            real_t lambda = this->avgPerCell;
+            real_t sum    = 0.0;
+
+            for ( auto k = 0; k < Worley2D::MAX_PER_CELL; k++ )
+            {
+                sum += std::pow( lambda,
+                                 static_cast<real_t>( k ) ) /
+                                 static_cast<real_t>( Util::Factorial( k ) );
+                real_t prob = std::exp( -1.0 * lambda ) * sum;
+
+                this->probTable[k] = Util::NormRealToU64( prob );
+            }
+        }
+        
+        virtual real_t getValue( real_t x, real_t y ) const override;
+
+        real_t getCustomValue(
+            real_t                                                        x,
+            real_t                                                        y,
+            std::function<real_t( std::array<real_t,
+                                             Worley2D::DISTANCES_SIZE> )> fn );
+
+    private:
+
+        static const std::size_t MAX_PER_CELL = 9;
+
+        uint64_t seed;
+        real_t   avgPerCell;
+        Distance distCalcMethod;
+
+        std::array<uint64_t, Worley2D::MAX_PER_CELL> probTable;
+
+        std::array<real_t, Worley2D::DISTANCES_SIZE> getValueHelper( real_t x, real_t y ) const;
+
+        std::size_t lookupNumFeaturePoints( uint64_t num ) const;
+    };
+    
+    class Worley3D : public Module3D
+    {
+    public:
+
+        static const std::size_t DISTANCES_SIZE = 4;
+
+        Worley3D( uint64_t seed           = 0,
+                  real_t   avgPerCell     = 2.0,
+                  Distance distCalcMethod = Distance::Euclidian ) :
+            seed( seed ),
+            avgPerCell( avgPerCell ),
+            distCalcMethod( distCalcMethod )
+        {
+            // Use the Cumulative Distribution Function of the Poisson Distribution
+            // to make a table on the odds of each cell having exactly n feature points.
+            // https://en.wikipedia.org/wiki/Poisson_distribution
+            real_t lambda = this->avgPerCell;
+            real_t sum    = 0.0;
+
+            for ( auto k = 0; k < Worley3D::MAX_PER_CELL; k++ )
+            {
+                sum += std::pow( lambda,
+                                 static_cast<real_t>( k ) ) /
+                                 static_cast<real_t>( Util::Factorial( k ) );
+                real_t prob = std::exp( -1.0 * lambda ) * sum;
+
+                this->probTable[k] = Util::NormRealToU64( prob );
+            }
+        }
+        
+        virtual real_t getValue( real_t x, real_t y, real_t z ) const override;
+
+        real_t getCustomValue(
+            real_t                                                        x,
+            real_t                                                        y,
+            real_t                                                        z,
+            std::function<real_t( std::array<real_t,
+                                             Worley3D::DISTANCES_SIZE> )> fn );
+
+    private:
+
+        static const std::size_t MAX_PER_CELL = 9;
+
+        uint64_t seed;
+        real_t   avgPerCell;
+        Distance distCalcMethod;
+
+        std::array<uint64_t, Worley3D::MAX_PER_CELL> probTable;
+
+        std::array<real_t, Worley3D::DISTANCES_SIZE> getValueHelper( real_t x, real_t y, real_t z ) const;
+
+        std::size_t lookupNumFeaturePoints( uint64_t num ) const;
+    };
+    
+    class Worley4D : public Module4D
+    {
+    public:
+
+        static const std::size_t DISTANCES_SIZE = 4;
+
+        Worley4D( uint64_t seed           = 0,
+                  real_t   avgPerCell     = 2.0,
+                  Distance distCalcMethod = Distance::Euclidian ) :
+            seed( seed ),
+            avgPerCell( avgPerCell ),
+            distCalcMethod( distCalcMethod )
+        {
+            // Use the Cumulative Distribution Function of the Poisson Distribution
+            // to make a table on the odds of each cell having exactly n feature points.
+            // https://en.wikipedia.org/wiki/Poisson_distribution
+            real_t lambda = this->avgPerCell;
+            real_t sum    = 0.0;
+
+            for ( auto k = 0; k < Worley4D::MAX_PER_CELL; k++ )
+            {
+                sum += std::pow( lambda,
+                                 static_cast<real_t>( k ) ) /
+                                 static_cast<real_t>( Util::Factorial( k ) );
+                real_t prob = std::exp( -1.0 * lambda ) * sum;
+
+                this->probTable[k] = Util::NormRealToU64( prob );
+            }
+        }
+        
+        virtual real_t getValue( real_t x, real_t y, real_t z, real_t w ) const override;
+
+        real_t getCustomValue(
+            real_t                                                        x,
+            real_t                                                        y,
+            real_t                                                        z,
+            real_t                                                        w,
+            std::function<real_t( std::array<real_t,
+                                             Worley4D::DISTANCES_SIZE> )> fn );
+
+    private:
+
+        static const std::size_t MAX_PER_CELL = 9;
+
+        uint64_t seed;
+        real_t   avgPerCell;
+        Distance distCalcMethod;
+
+        std::array<uint64_t, Worley4D::MAX_PER_CELL> probTable;
+
+        std::array<real_t, Worley4D::DISTANCES_SIZE> getValueHelper( real_t x, real_t y, real_t z, real_t w ) const;
+
+        std::size_t lookupNumFeaturePoints( uint64_t num ) const;
+    };
 }
